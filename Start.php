ea6b937ed398ea1acc7b2b35901ec58a7d27c807
Luka Agic <agic.luke@gmail.com>
2023-01-31 22:01:11 +0100

    Initial commit
    Display greeting message and main menu options

---
+++ Start.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class Start2 {
+
+    public function __construct() {
+        $this->getMessage();
+        $this->displayMainMenu();
+    }
+
+    public function getMessage(){
+        echo 'Greetings, welcome to the Voyager hospital' . PHP_EOL;
+    }
+
+    public function displayMainMenu(){
+        echo 'This is the main menu, choose an option: ' . PHP_EOL;
+        echo '1. View visitor appointments' . PHP_EOL;
+        echo '2. View visiting records' . PHP_EOL;
+        echo '3. View patients' . PHP_EOL;
+        echo '4. View doctors' . PHP_EOL;
+        echo '5. View departments' . PHP_EOL;
+        echo '6. View medical records' . PHP_EOL;
+        echo '7. Exit terminal app' . PHP_EOL;
+
+
+    }
+}
+
+new Start2;
